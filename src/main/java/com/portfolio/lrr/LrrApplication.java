c7b25ea218d3b8bebd4f6d24d8d24899852920b1
LucianaRosso <111255607+LucianaRosso@users.noreply.github.com>
2023-03-26 19:19:49 -0300

    Tercera versión para deploy

--- src/main/java/com/portfolio/lrr/LrrApplication.java
+++ src/main/java/com/portfolio/lrr/LrrApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LrrApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LrrApplication.class, args);
 	}
