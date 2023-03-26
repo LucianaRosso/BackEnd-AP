3b9d92c4a94d6f7859ba1e6397bc6f2843cf477d
LucianaRosso <111255607+LucianaRosso@users.noreply.github.com>
2023-03-26 17:24:42 -0300

    VAMAAA

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM amazoncorretto:17
+MAINTAINER LRR 
+COPY target/lrr-0.0.1-SNAPSHOT.jar    lrr-app.jar           
+ENTRYPOINT ["java","-jar","/lrr-app.jar"]
---
+++ src/main/java/com/portfolio/lrr/Service/ImpPersonaService.java
@@ -0,0 +1,44 @@
+
+package com.portfolio.lrr.Service;
+
+import com.portfolio.lrr.Entity.Persona;
+import com.portfolio.lrr.Repository.IPersonaRepository;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Transactional
+@Service
+public class ImpPersonaService{
+    @Autowired IPersonaRepository ipersonaRepository;
+    
+    public List<Persona> list(){
+         return ipersonaRepository.findAll();
+     }
+     
+     public Optional<Persona> getOne(int id){
+         return ipersonaRepository.findById(id);
+     }
+     
+     public Optional<Persona> getByNombre(String nombre){
+         return ipersonaRepository.findByNombre(nombre);
+     }
+     
+     public void save(Persona persona){
+         ipersonaRepository.save(persona);
+     }
+     
+     public void delete(int id){
+         ipersonaRepository.deleteById(id);
+     }
+     
+     public boolean existsById(int id){
+         return ipersonaRepository.existsById(id);
+     }
+     
+     public boolean existsByNombre(String nombre){
+         return ipersonaRepository.existsByNombre(nombre);
+     }
+}
