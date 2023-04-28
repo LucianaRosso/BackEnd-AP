968d8bbc318b65b25e16d213de9ccb14d91bcd14
LucianaRosso <111255607+LucianaRosso@users.noreply.github.com>
2023-04-27 22:35:57 -0300

    V26

--- src/main/java/com/portfolio/lrr/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/lrr/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "**")
+@CrossOrigin(origins = {"https://fronend-513d9.web.app","http://localhost:4200"})
 public class CExperiencia {
      @Autowired
     SExperiencia sExperiencia;
