0ee711f89438dee6466a3156a418e870ba2a90f4
LucianaRosso <111255607+LucianaRosso@users.noreply.github.com>
2023-04-27 22:00:44 -0300

    controllers

--- src/main/java/com/portfolio/lrr/WebConfig.java
+++ src/main/java/com/portfolio/lrr/WebConfig.java
@@ -5,3 +5,16 @@
 package com.portfolio.lrr;
 
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+ @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");
+    }
+}
